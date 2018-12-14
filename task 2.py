13b2208ee499c446bf6c91eceb21381c6c04484d
AshiqulIslam99 <44380076+AshiqulIslam99@users.noreply.github.com>
2018-12-14 15:54:32 +0000

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,13 @@
+n_lines = int(input("How many lines? "))
+counter = 1
+
+
+while n_lines >= counter:
+    print("This is line " + str(counter))
+    counter += 1
+
+    
+    # print("This is line " + str(counter))
+
+print()
+input("Press return to continue ...")
